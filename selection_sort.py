a61a15d53fd806fbe4c752c7151e59bcde13efbf
MasiaMatteo03 <72200972+MasiaMatteo03@users.noreply.github.com>
2020-11-08 16:34:29 +0100

    Add files via upload

---
+++ selection_sort.py
@@ -0,0 +1,15 @@
+vett = [1,2,8,4,5,10,7,3,9,6]
+print("\nVettore iniziale")
+print(vett)
+
+#algoritmo selection sort ordinamento vettore vett
+for cnt in range (len(vett)):
+    cntMin = cnt
+    for i in range(cnt + 1, len(vett)):
+        if vett[cntMin] > vett[i]:
+            cntMin = cnt
+            temp = vett[i]
+            vett[i] = vett[cntMin]
+            vett[cntMin] = temp
+print("\nVettore ordinato")
+print(vett)
